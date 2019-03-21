af72ed750ed64aa82ab5c1d476e0d35237c2809c
raphaelw12 <name>
2019-03-21 16:48:27 -0300

    pke

--- client/main.js
+++ client/main.js
@@ -1,22 +1,18 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+import'../lib/collection.js';
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.myJumbo.events({
+	'click .js-addImg'(event){
+		$("#addImgModal").modal("show");
+	}
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.addImg.events({
+	'click .js-saveImg'(event){
+		console.log("save");
+		$("#addImgModal").modal("hide");	
+	}
 });
